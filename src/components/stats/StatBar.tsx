02a6495b834c65eea0235689a5b0225d1f640f5e
Miklos Danka <mdanka@users.noreply.github.com>
2022-01-22 15:20:30 +0100

    Translation

--- src/components/stats/StatBar.tsx
+++ src/components/stats/StatBar.tsx
@@ -22,10 +22,10 @@ const StatItem = ({
 export const StatBar = ({ gameStats }: Props) => {
   return (
     <div className="flex justify-center my-2">
-      <StatItem label="Total tries" value={gameStats.totalGames} />
-      <StatItem label="Success rate" value={`${gameStats.successRate}%`} />
-      <StatItem label="Current streak" value={gameStats.currentStreak} />
-      <StatItem label="Best streak" value={gameStats.bestStreak} />
+      <StatItem label="Összes játék" value={gameStats.totalGames} />
+      <StatItem label="Sikerráta" value={`${gameStats.successRate}%`} />
+      <StatItem label="Jelenlegi folyamatos siker" value={gameStats.currentStreak} />
+      <StatItem label="Leghosszabb folyamatos siker" value={gameStats.bestStreak} />
     </div>
   )
 }
